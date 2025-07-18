ebaac03d02bfefd4625876687b927f71ed723cff
Stefan Albu <stefanalbu@hotmail.com>
2025-07-19 00:57:45 +0100

    fix: update azurerm provdier to version 4+

--- main.tf
+++ main.tf
@@ -8,9 +8,10 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~> 3.0"
+      version = "~> 4.0"
     }
   }
+  required_version = ">= 1.3"
 }
 
 provider "azurerm" {
